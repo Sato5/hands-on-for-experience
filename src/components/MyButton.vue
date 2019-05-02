dcc6afcdfddc96e2fc12a614a5170a63c2f397f6
naporitan <naporin0624@gmail.com>
2019-05-02 20:58:44 +0900

    button.vue

---
+++ src/components/MyButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="my-button" @click.end="clickFunc">
+    <!-- slotはコンポーネントの間に入れたDOMが入るところ -->
+    <!-- <my-button>おしてね</my-button> -->
+    <!-- 上の例だとボタンの中におしてねが書かれた状態でレンダリングされます -->
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+  methods: {
+    clickFunc (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-button {
+  border: none;
+  padding: 8px 32px 8px 32px;
+  border-radius: 8px;
+}
+.my-button:hover {
+  background-color: #BDBDBD
+}
+.my-button:active {
+  background-color: #757575
+}
+</style>
